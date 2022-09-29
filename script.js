680874790506f8ef44e6ca38a16e944bb93a5aa9
Evv3n <96411992+Evv3n@users.noreply.github.com>
2022-09-29 22:39:48 +0200

    played around
    tried fixing hamburger button half way there

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const strengthNumber = document.getElementById("strengthNumbers");
 const strengthClickReg = document.querySelector("#strengthClickReg");
-
+const activeHamburger = document.querySelector('hamburger.is-active')
 
 var count = 0;
 
@@ -16,14 +16,14 @@ const incrementCount = document.getElementById('strengthbutton');
 
 incrementCount.addEventListener("click", handleIncrement);
 
-hamburger.addEventListener('click', function(){
-  this.classList.toggle('is-active');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('is-active');
   document.getElementById('menu1').style.display='flex';
 });
-if (this.classList = '') {
+
+activeHamburger.addEventListener('click', () => {
   document.getElementById('menu1').style.display='none';
-  console.log('dfgdfh')
-}
+})
 
 
 
