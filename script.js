47ab2b227faf1407d4d8dd860faa06b698097063
Evv3n <eivind.andreas.hans1@gmail.com>
2022-09-30 13:47:40 +0200

    i added small stuff

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const hamburger = document.querySelector('.hamburger');
 const strengthNumber = document.getElementById("strengthNumbers");
 const strengthClickReg = document.querySelector("#strengthClickReg");
-const activeHamburger = document.querySelector('hamburger.is-active')
+const activeHamburger = document.querySelector('#hamburger.is-active')
+const menu1 = document.querySelector('#menu1');
+const pwoimgtest = document.getElementById('pwoimgtest');
+const price_for_pwo = document.getElementById('pwo1-price')
 
-var count = 0;
+var strength = 0;
+var passive_income = 0;
+//Prices
+var price_for_pwo1 = 15;
 
-strengthNumber.innerHTML = 'Strength: ' + count;
+
+
+strengthNumber.innerHTML = 'Strength: ' + strength;
 
 const handleIncrement = () => {
-  count++;
-  strengthNumber.innerHTML = 'Strength: ' + count;
+  strength++;
+  strengthNumber.innerHTML = 'Strength: ' + strength;
 }
 
 const incrementCount = document.getElementById('strengthbutton');
@@ -18,21 +26,40 @@ incrementCount.addEventListener("click", handleIncrement);
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('is-active');
-  document.getElementById('menu1').style.display='flex';
+  menu1.style.display = 'flex'
 });
 
+pwoimgtest.addEventListener('click', buy_pwo);
+
+function buy_pwo(){
+  if(strength > price_for_pwo1){
+    passive_income++;
+    strength -= price_for_pwo1;
+  } else {
+    
+  }
+} 
+
+
+var update_strenght = setInterval(myTimer, 10);
+function myTimer(){
+  strength += passive_income;
+  strengthNumber.innerHTML = 'Strength: ' + strength;
+}
+
+/*
+var passive_timer = setInterval(myTimer, 10);
+function myTimer(){
+  strength += passive_income;
+}
+
+
+
+/*
 activeHamburger.addEventListener('click', () => {
-  document.getElementById('menu1').style.display='none';
+  menu1.style.display = 'none'
 })
-
-
-
-
-//if (count > 0) {
-  //strengthClickReg.style.visibility = 'visible';
-  //console.log('registered')
-//};
-
+*/
 
 
 
