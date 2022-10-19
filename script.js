59b74e2c9d8ce61e89edf5e38ab7efdc0df9f652
Evv3n <eivind.andreas.hans1@gmail.com>
2022-10-19 15:09:31 +0200

    Some changes i dont remeber lol

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 const hamburger = document.querySelector('.hamburger');
 const strengthNumber = document.getElementById("strengthNumbers");
-const strengthPerSecond = document.querySelector(".strengthPerSecond")
+const strengthPerSecond = document.querySelector(".strengthPerSecond");
 const strengthClickReg = document.querySelector("#strengthClickReg");
-const activeHamburger = document.querySelector('#hamburger.is-active')
+const activeHamburger = document.querySelector('#hamburger.is-active');
 const menu1 = document.querySelector('.menu');
 const pwoimgtest = document.getElementById('pwoimgtest');
-const price_for_pwo = document.getElementById('pwo1-price')
-const settings = document.getElementById('settings')
-const settingsa = document.getElementById('settingsa')
-const achievementsa = document.getElementById('achievementsa')
-const achievements = document.getElementById('achievements')
-const statsa = document.getElementById('statsa')
-const trainersa = document.getElementById('trainersa')
-const stats = document.getElementById('stats')
-const trainers = document.getElementById('trainers')
+const pwoimgtest2 = document.getElementById('pwoimgtest2');
+const pwoimgtest3 = document.getElementById('pwoimgtest3');
+const price_for_pwo = document.getElementById('pwo1-price');
+const settings = document.getElementById('settings');
+const settingsa = document.getElementById('settingsa');
+const achievementsa = document.getElementById('achievementsa');
+const achievements = document.getElementById('achievements');
+const statsa = document.getElementById('statsa');
+const trainersa = document.getElementById('trainersa');
+const stats = document.getElementById('stats');
+const trainers = document.getElementById('trainers');
+const strengthbutton = document.querySelector('#strengthbutton');
 
-
-var strength = 0;
+var strength = 100000;
 var passive_income = 0;
 //Prices
-var price_for_pwo1 = 15;
+var price_for_pwo1 = 100;
+var price_for_pwo2 = 1000;
+var price_for_pwo3 = 10000;
 
 const handleIncrement = () => {
   strength++;
@@ -43,7 +47,8 @@ function isActive() {
 
 
 pwoimgtest.addEventListener('click', buy_pwo);
-
+pwoimgtest2.addEventListener('click', buy_pwo2);
+pwoimgtest3.addEventListener('click', buy_pwo3);
 settingsa.addEventListener('click', settings_open)
 achievementsa.addEventListener('click', achievements_open)
 statsa.addEventListener('click', stats_open)
@@ -69,8 +74,22 @@ function buy_pwo(){
     pwoimgtest.style.display = 'none';
   }
 }   
-
-
+function buy_pwo2(){ 
+  if(strength > price_for_pwo2){
+    passive_income += price_for_pwo2;
+    strength -= price_for_pwo2;
+    strengthNumber.innerHTML = 'Strength: ' + strength;
+    pwoimgtest2.style.display = 'none';
+  }
+}   
+function buy_pwo3(){ 
+  if(strength > price_for_pwo3){
+    passive_income += price_for_pwo3;
+    strength -= price_for_pwo3;
+    strengthNumber.innerHTML = 'Strength: ' + strength;
+    pwoimgtest3.style.display = 'none';
+  }
+}   
 var update_strenght = setInterval(myTimer, 1000);
 function myTimer(){
   strength += passive_income;
